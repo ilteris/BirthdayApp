4e4347cfb6037ec5af1334661e8736dfdf1d6c63
ilteris <ilteris@gmail.com>
2013-10-31 20:16:14 -0400

    add parse method to save the user profile object to parse

--- BirthdayApp/BDAListViewController.m
+++ BirthdayApp/BDAListViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "BDAListViewController.h"
+#import <Parse/Parse.h>
 
 @interface BDAListViewController ()
 
@@ -27,6 +28,63 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    // Send request to Facebook
+    FBRequest *request = [FBRequest requestForMe];
+    [request startWithCompletionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
+        // handle response
+        if (!error) {
+            // Parse the data received
+            NSDictionary *userData = (NSDictionary *)result;
+            
+            NSString *facebookID = userData[@"id"];
+            
+            NSURL *pictureURL = [NSURL URLWithString:[NSString stringWithFormat:@"https://graph.facebook.com/%@/picture?type=large&return_ssl_resources=1", facebookID]];
+            
+            NSMutableDictionary *userProfile = [NSMutableDictionary dictionaryWithCapacity:7];
+            
+            if (facebookID) {
+                userProfile[@"facebookId"] = facebookID;
+            }
+            
+            if (userData[@"name"]) {
+                userProfile[@"name"] = userData[@"name"];
+            }
+            
+            if (userData[@"location"][@"name"]) {
+                userProfile[@"location"] = userData[@"location"][@"name"];
+            }
+            
+            if (userData[@"gender"]) {
+                userProfile[@"gender"] = userData[@"gender"];
+            }
+            
+            if (userData[@"birthday"]) {
+                userProfile[@"birthday"] = userData[@"birthday"];
+            }
+            
+            if (userData[@"relationship_status"]) {
+                userProfile[@"relationship"] = userData[@"relationship_status"];
+            }
+            
+            if ([pictureURL absoluteString]) {
+                userProfile[@"pictureURL"] = [pictureURL absoluteString];
+            }
+            
+            [[PFUser currentUser] setObject:userProfile forKey:@"profile"];
+            [[PFUser currentUser] saveInBackground];
+            
+            
+        } else if ([[[[error userInfo] objectForKey:@"error"] objectForKey:@"type"]
+                    isEqualToString: @"OAuthException"]) { // Since the request failed, we can check if it was due to an invalid session
+            NSLog(@"The facebook session was invalidated");
+           
+        } else {
+            NSLog(@"Some other error: %@", error);
+        }
+    }];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
