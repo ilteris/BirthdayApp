8a895d468faf15f69f5be7a10561bd6d8211d245
ilteris <ilteris@gmail.com>
2013-10-31 22:24:00 -0400

    add the single dispatch init, containers for holding fbfriends list

--- BirthdayApp/BDADataSource.m
+++ BirthdayApp/BDADataSource.m
@@ -10,4 +10,35 @@
 
 @implementation BDADataSource
 
+NSMutableDictionary* _fbFriends;
+NSMutableArray* _birthdayOfFriends;
+
++(id)sharedInstance {
+    static dispatch_once_t p = 0;
+    
+    __strong static id _sharedObject = nil;
+    
+    dispatch_once(&p, ^{
+        _sharedObject = [[self alloc] init];
+    });
+    
+    return _sharedObject;
+}
+
+
+- (id)init {
+    self =[super init];
+    if (self) {
+        //initialize Default Data here.
+        [self initializeDefaultDataList];
+    }
+    return self;
+}
+
+- (void)initializeDefaultDataList {
+    _fbFriends = [[NSMutableDictionary alloc] init];
+    _birthdayOfFriends = [[NSMutableArray alloc] init];
+    
+}
+
 @end
