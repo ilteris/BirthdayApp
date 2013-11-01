72a7b1772f1f5bdfeac0380bfa95ea532accd36f
ilteris <ilteris@gmail.com>
2013-10-31 22:17:00 -0400

    Add datasource

---
+++ BirthdayApp/BDADataSource.h
@@ -0,0 +1,13 @@
+//
+//  BDADataSource.h
+//  BirthdayApp
+//
+//  Created by ilteris on 10/31/13.
+//  Copyright (c) 2013 ilteris. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BDADataSource : NSObject
+
+@end
---
+++ BirthdayApp/BDADataSource.m
@@ -0,0 +1,13 @@
+//
+//  BDADataSource.m
+//  BirthdayApp
+//
+//  Created by ilteris on 10/31/13.
+//  Copyright (c) 2013 ilteris. All rights reserved.
+//
+
+#import "BDADataSource.h"
+
+@implementation BDADataSource
+
+@end
