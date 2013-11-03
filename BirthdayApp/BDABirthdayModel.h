380de1f93442c12f1c0955ffb4a89f37de59501c
ilteris <ilteris@gmail.com>
2013-11-03 17:02:57 -0500

    add birthday model

---
+++ BirthdayApp/BDABirthdayModel.h
@@ -0,0 +1,14 @@
+//
+//  BDABirthdayModel.h
+//  BirthdayApp
+//
+//  Created by ilteris on 11/3/13.
+//  Copyright (c) 2013 ilteris. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BDABirthdayModel : NSObject
+@property (nonatomic, strong) NSDictionary<FBGraphUser> *user;
+
+@end
---
+++ BirthdayApp/BDABirthdayModel.m
@@ -0,0 +1,13 @@
+//
+//  BDABirthdayModel.m
+//  BirthdayApp
+//
+//  Created by ilteris on 11/3/13.
+//  Copyright (c) 2013 ilteris. All rights reserved.
+//
+
+#import "BDABirthdayModel.h"
+
+@implementation BDABirthdayModel
+
+@end
